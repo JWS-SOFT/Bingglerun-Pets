35d99f388bd6c283e24b8debe6f5624f2538fe55
Mained606 <mained606@gmail.com>
2025-05-09 06:10:21 +0900

    Revert "게스트 로그인 메일 로그인 테스트"
    This reverts commit 0fbf712f765bcc2949518c0bfffee80e5d9d461c.

--- Assets/Bigglerun_Pets/WorkPlace/Mained606/606Scripts/TitleUIController.cs
+++ Assets/Bigglerun_Pets/WorkPlace/Mained606/606Scripts/TitleUIController.cs
@@ -1,58 +1,33 @@
 using UnityEngine;
 using UnityEngine.UI;
-using Firebase.Auth;
 
 public class TitleUIController : MonoBehaviour
 {
-    [SerializeField] private Button guestLoginButton;
-    [SerializeField] private Button emailLoginButton;
-    [SerializeField] private Button googleLoginButton;
+    [SerializeField] private Button loginButton;
 
     private void Start()
     {
-        guestLoginButton.onClick.AddListener(OnGuestLogin);
-        emailLoginButton.onClick.AddListener(OnEmailLogin);
-        googleLoginButton.onClick.AddListener(OnGoogleLogin);
+        loginButton.onClick.AddListener(OnClickLogin);
     }
 
-    private async void OnGuestLogin()
+    private async void OnClickLogin()
     {
-        guestLoginButton.interactable = false;
+        loginButton.interactable = false;
 
-        bool success = await FirebaseManager.Instance.SignInAnonymously();
+        bool success = await FirebaseManager.Instance.InitializeAndLoginAsync();
+
         if (success)
         {
-            GameManager.Instance.SceneFader.LoadScene("Lobby");
+            // 씬 전환 → 로비로
+            GameManager.Instance.SceneFader.LoadScene("LobbyScene");
+
+            // 상태 전환은 씬 로딩 이후 Start 등에서 처리하거나, 여기서 즉시
             GameManager.Instance.StateMachine.ChangeState(GameState.Lobby);
         }
         else
         {
-            guestLoginButton.interactable = true;
+            Debug.LogWarning("로그인 실패");
+            loginButton.interactable = true;
         }
-    }
-
-    private async void OnEmailLogin()
-    {
-        emailLoginButton.interactable = false;
-
-        // TODO: 이메일/비밀번호 입력창 연결
-        string email = "test@example.com";     // 임시 입력값
-        string password = "123456";            // 임시 입력값
-
-        bool success = await FirebaseManager.Instance.SignInWithEmail(email, password);
-        if (success)
-        {
-            GameManager.Instance.SceneFader.LoadScene("Lobby");
-            GameManager.Instance.StateMachine.ChangeState(GameState.Lobby);
-        }
-        else
-        {
-            emailLoginButton.interactable = true;
-        }
-    }
-
-    private void OnGoogleLogin()
-    {
-        Debug.Log("구글 로그인은 추후 지원 예정입니다.");
     }
 }
