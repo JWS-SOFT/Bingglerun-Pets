72101fbe1e30bbd9a2e81a5fb1fafaac20f99289
Saga1973 <jokerr07@naver.com>
2025-05-12 19:48:34 +0900

    랜덤코인 생성
    랜덤코인 생성

--- Assets/Bigglerun_Pets/WorkPlace/JWS/Scripts/CameraFollow.cs
+++ Assets/Bigglerun_Pets/WorkPlace/JWS/Scripts/CameraFollow.cs
@@ -5,11 +5,11 @@ public class CameraFollow : MonoBehaviour
     public Transform player;
     public float followSpeed = 2f;
 
-    void LateUpdate()
+    private void LateUpdate()
     {
         if (player != null)
         {
-            Vector3 targetPos = new Vector3(0, player.position.y + 3f, -10f);
+            Vector3 targetPos = new Vector3(player.position.x, player.position.y + 3f, -10f);
             transform.position = Vector3.Lerp(transform.position, targetPos, followSpeed * Time.deltaTime);
         }
     }
