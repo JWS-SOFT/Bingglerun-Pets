a274dfaf25c370a83ed5fa84d9e96263d63951a8
iri-sh326 <bf980326@gmail.com>
2025-05-15 14:29:58 +0900

    데코레이팅 창에서 데코아이템 장착
    구매해서 unlock된 아이템만 장착 가능
    부위별 장착
    (아직 탈착 불가능)

--- Assets/Bigglerun_Pets/WorkPlace/SH/Scripts/UIScripts/DecoratingUI.cs
+++ Assets/Bigglerun_Pets/WorkPlace/SH/Scripts/UIScripts/DecoratingUI.cs
@@ -1,11 +1,16 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using static UnityEditor.Progress;
 
 public class DecoratingUI : MonoBehaviour
 {
+    private DecorationItemData decoItemData;
     private PlayerDataManager playerData;
     [SerializeField] private TextMeshProUGUI playerGold;
     [SerializeField] private TextMeshProUGUI playerCash;
+
+    private int selectedItemIndex;
 
     private void Awake()
     {
@@ -35,5 +40,50 @@ public class DecoratingUI : MonoBehaviour
     private void SetDiamondData(int diamond)
     {
         playerCash.text = diamond.ToString();
+    }
+
+    private void ItemSelect()
+    {
+        GameObject clickedObj = EventSystem.current.currentSelectedGameObject;
+
+        // 부모인 Content Transform
+        Transform parent = clickedObj.transform.parent;
+
+        // 클릭된 버튼이 Content에서 몇 번째 자식인지 찾기
+        selectedItemIndex = clickedObj.transform.GetSiblingIndex();
+
+        Debug.Log("클릭된 버튼의 인덱스: " + selectedItemIndex);
+        decoItemData = parent.GetChild(selectedItemIndex).GetComponent<ShItem>().decoItemData;
+    }
+
+    public void EquipButton()
+    {
+        ItemSelect();
+        bool unlocked = ItemManager.Instance.IsUnlockedDecoration(decoItemData.itemId);
+        if (unlocked)
+        {
+            ItemManager.Instance.EquipDecoration(decoItemData.itemId);
+            DecorationItemData deco;
+            switch (decoItemData.type)
+            {
+                case DecorationType.Hat:
+                    deco = ItemManager.Instance.GetEquippedDecoration(DecorationType.Hat);
+                    break;
+                case DecorationType.Body:
+                    deco = ItemManager.Instance.GetEquippedDecoration(DecorationType.Body);
+                    break;
+                case DecorationType.Shoes:
+                    deco = ItemManager.Instance.GetEquippedDecoration(DecorationType.Shoes);
+                    break;
+                default:
+                    deco = ItemManager.Instance.GetEquippedDecoration(DecorationType.Hat);
+                    break;
+            }
+            Debug.Log($"데코 아이템 장착됨: {deco.itemName}");
+        }
+        else
+        {
+            Debug.Log("데코 아이템 언락되지 않음");
+        }
     }
 }
