24d8d5e49aa0b6b2bb771298ab33ca4ffd2ec69a
iri-sh326 <bf980326@gmail.com>
2025-05-22 16:52:32 +0900

    게임모드에 따라 카메라 팔로우 속도 조절
    currentSpeed 변수 추가

--- Assets/Bigglerun_Pets/WorkPlace/JWS/Scripts/CameraFollow.cs
+++ Assets/Bigglerun_Pets/WorkPlace/JWS/Scripts/CameraFollow.cs
@@ -4,6 +4,7 @@ public class CameraFollow : MonoBehaviour
 {
     private Transform player;
     public float followSpeed = 3f;
+    private float currentSpeed;
 
     public bool mode = false;
     private Vector3 targetPos;
@@ -16,18 +17,19 @@ public class CameraFollow : MonoBehaviour
         if (PlayerManager.PlayMode)
         {
             // ➤ 횡스크롤 모드 (y는 고정, x만 따라감)
-            if (PlayerManager.Instance.isGameStartReady) followSpeed *= 0;
+            if (PlayerManager.Instance.isGameStartReady) currentSpeed = 0;
             targetPos = new Vector3(player.position.x + 1.5f, player.position.y + 2.5f, -10f);
         }
         else
         {
             // ➤ 계단 모드 (y도 따라감, 살짝 위를 보게)
+            currentSpeed = followSpeed;
             targetPos = new Vector3(player.position.x, player.position.y + 1f, -10f);
         }
     }
 
     private void LateUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, targetPos, followSpeed * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, targetPos, currentSpeed * Time.deltaTime);
     }
 }
